025ce4a06ea207415b2a0b3e96dd209c75af1384
mossin01 <01lyadimossin@gmail.com>
2024-01-30 16:41:24 +0100

    lezione 1

---
+++ lezione1/Esercizioinput.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+/*public class Esercizioninput {
+
+    public static void main(String args[]){
+      Scanner tastiera = new Scanner(System.in);
+      System.out.print("Inserisci un nome: ");
+       String nome = tastiera.nextLine();
+       System.out.print("Inserisci un cognome: ");
+      String cognome = tastiera.nextLine();
+      System.out.print("Inserisci un eta : ");
+      int eta = Integer.parseInt(tastiera.nextLine());
+      System.out.println("Ciao  "   + nome + " " + cognome+" "+ (eta*2));
+      */
+      
+
+
+        System.out.print("Inserisci una base: ");
+         int base = Integer.parseInt(tastiera.nextLine());
+
+        System.out.print("Inserisci un altezza: ");
+          int altezza = Integer.parseInt(tastiera.nextLine());
+
+          System.out.println("l'area del rettangolo  " + base + " " + altezza)
+
+
+
+
+    }
+}
---
+++ lezione1/Operazioni.java
@@ -0,0 +1,14 @@
+public class Operazioni{
+    public static void main(String[] args){
+
+int a = 10;
+int b = 20;
+int c = 15;
+double d = 25.5;
+System.out.println(a+b);
+System.out.println(b-a);
+System.out.println(c/a);
+System.out.println(c*d);
+System.out.println(c%d);
+}
+    }
